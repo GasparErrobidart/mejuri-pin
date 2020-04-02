a0f9504d26e9f5dc942b2f09b52b28911b7b0193
GasparErrobidart <podmaster@hotmail.com>
2020-04-02 14:31:18 -0300

    Add SW for PWA

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,18 @@ export default class MyDocument extends Document {
       <html>
         <Head>
           <title>Mejuri Pin</title>
+          <meta name='application-name' content='Mejuri Pin App' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='App' />
+          <meta name='description' content='Best App in the world' />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <meta name='msapplication-tap-highlight' content='no' />
+          <meta name='theme-color' content='#000000' />
+          <link rel='manifest' href='/manifest.json' />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
           <script src="/picture-perfect.min.js"></script>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
